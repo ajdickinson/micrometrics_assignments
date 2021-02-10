822c4b3af7ac4ed113cf54ef24b5fe95659edb45
Andrew Dickinson <adickin3@uoregon.edu>
2021-02-10 13:07:27 -0800

    A5 added

--- A4/tmp1.R
+++ A4/tmp1.R
@@ -7,35 +7,40 @@ rho = 1
 iter = 1
 
 
-# individual level 
-Sigma_i <- matrix(c(1, 0, 0, 1 - rho), ncol = 2)
-values_i <- rmvnorm(n = n, sigma = Sigma_i)
-# cluster level
-cluster_name <- rep(1:n_cluster, each = n / n_cluster)
-Sigma_cl <- matrix(c(1, 0, 0, rho), ncol = 2)
-values_cl <- rmvnorm(n = n_cluster, sigma = Sigma_cl)
-# error consists of individual- and cluster-level components
-e1 <- values_i[ , 1] + rep(values_cl[ , 1], each = n / n_cluster)
-e2 <- values_i[ , 2] + rep(values_cl[ , 2], each = n / n_cluster)
-# now introduce treatment that varies across i within c and across c
-treat_ish = values_i[,1] + rep(values_cl[ , 2], each = n_obs / n_cluster)
-treat = ifelse(treat_ish > 1, 1, 0)
-# data generating process
-y <- 1 + treat + e1 + e2 
-d_iter <- tibble(cluster = cluster_name, e1, e2, y, treat)
-# regress y on treat
-m = feols(data = d_iter, y ~ treat)
-m1 = m %>% tidy()
-m2 = m %>% tidy(cluster = c("cluster"))
-# m1 = lm_robust(y ~ treat, data = d_iter, clusters = cluster) %>% tidy()
-# m2 = lm_robust(y ~ treat, data = d_iter, clusters = cluster) %>% tidy()
-# save model estimates
-bind_rows(m1, m2) %>%
-  select(1:5) %>% filter(term == "treat") %>%
-  mutate(diff = estimate[1] - estimate[2],
-         lower = estimate - 1.96*(std.error/sqrt(n_obs)),
-         upper = estimate + 1.96*(std.error/sqrt(n_obs)),
-         se_type = c("normal", "clustered"),
+## individual level
+Sigma_i = matrix(c(1, 0, 0, 1 - rho), ncol = 2)
+values_i = rmvnorm(n = n_obs, sigma = Sigma_i)
+## cluster level
+cluster_name = rep(1:n_cluster, each = n_obs / n_cluster)
+Sigma_cl = matrix(c(1, 0, 0, rho), ncol = 2)
+values_cl = rmvnorm(n = n_cluster, sigma = Sigma_cl)
+d_iter = tibble(
+  cluster = cluster_name,
+  ## error consists of individual- and cluster-level components
+  e1 = values_i[ , 1] + rep(values_cl[ , 1], each = n_obs / n_cluster),
+  e2 = values_i[ , 2] + rep(values_cl[ , 2], each = n_obs / n_cluster),
+  ## now introduce treatment that varies across i
+  treat_i = sample(c(0,1), n_obs, replace = TRUE),
+  treat_c = rep(0:1, each = n_obs / n_cluster, len = n_obs),
+  ## data generating process for both treatment levels i and c
+  y_i = 1 + treat_i + e1 + e2,
+  y_c = 1 + treat_c + e1 + e2
+)
+## run each model; treatment at i and c
+m_i = feols(data = d_iter, y_i ~ treat_i)
+m_c = feols(data = d_iter, y_c ~ treat_c)
+## package into single data frame that will output into a list
+m_i1 = m_i %>% tidy()
+m_i2 = m_i %>% tidy(cluster = c("cluster"))
+m_c1 = m_c %>% tidy()
+m_c2 = m_c %>% tidy(cluster = c("cluster"))
+bind_rows(m_i1, m_i2, m_c1, m_c2) %>%
+  select(1:5) %>% filter(term %in% c("treat_i", "treat_c")) %>%
+  mutate(diff_i = estimate[1] - estimate[2],
+         diff_c = estimate[3] - estimate[4],
+         lower = -1.96*(std.error/sqrt(n_obs)),
+         upper = 1.96*(std.error/sqrt(n_obs)),
+         se_type = c("normal", "clustered","normal", "clustered"),
          i = iter,
          rho = rho)
 
--- A4/tmp3.R
+++ A4/tmp3.R
@@ -7,41 +7,46 @@ set.seed(123)
 
 cluster_iter = function(iter = 1, n_obs = 1000, n_cluster = 20) {
   rho = runif(n = 1, min = 0, max = 1)
-  # individual level
+  ## individual level
   Sigma_i = matrix(c(1, 0, 0, 1 - rho), ncol = 2)
   values_i = rmvnorm(n = n_obs, sigma = Sigma_i)
-  # cluster level
+  ## cluster level
   cluster_name = rep(1:n_cluster, each = n_obs / n_cluster)
   Sigma_cl = matrix(c(1, 0, 0, rho), ncol = 2)
   values_cl = rmvnorm(n = n_cluster, sigma = Sigma_cl)
   d_iter = tibble(
     cluster = cluster_name,
-    # error consists of individual- and cluster-level components
+    ## error consists of individual- and cluster-level components
     e1 = values_i[ , 1] + rep(values_cl[ , 1], each = n_obs / n_cluster),
     e2 = values_i[ , 2] + rep(values_cl[ , 2], each = n_obs / n_cluster),
-    # now introduce treatment that varies across i within c and across c
-    treat = values_i[,1] + rep(values_cl[ , 2], each = n_obs / n_cluster),
-    # data generating process
-    y = 1 + treat + e1 + e2 
+    ## now introduce treatment that varies across i
+    treat_i = rep(0:1, each = n_obs / 2),
+    treat_c = rep(0:1, each = n_obs / n_cluster, len = n_obs),
+    ## data generating process for both treatment levels i and c
+    y_i = 1 + treat_i + e1 + e2,
+    y_c = 1 + treat_c + e1 + e2
   )
-  m = feols(data = d_iter, y ~ treat)
-  m1 = m %>% tidy()
-  m2 = m %>% tidy(cluster = c("cluster"))
-  # m1 <- lm_robust(y ~ treat, data = d_iter) %>% tidy()
-  # m2 <- lm_robust(y ~ treat, data = d_iter, clusters = cluster) %>% tidy()
-  # save model estimates
-  ret = bind_rows(m1, m2) %>%
-    select(1:5) %>% filter(term == "treat") %>%
-    mutate(diff = estimate[1] - estimate[2],
+  ## run each model; treatment at i and c
+  m_i = feols(data = d_iter, y_i ~ treat_i)
+  m_c = feols(data = d_iter, y_c ~ treat_c)
+  ## package into single data frame that will output into a list
+  m_i1 = m_i %>% tidy()
+  m_i2 = m_i %>% tidy(cluster = c("cluster"))
+  m_c1 = m_c %>% tidy()
+  m_c2 = m_c %>% tidy(cluster = c("cluster"))
+  ret = bind_rows(m_i1, m_i2, m_c1, m_c2) %>%
+    select(1:5) %>% filter(term %in% c("treat_i", "treat_c")) %>%
+    mutate(diff_i = estimate[1] - estimate[2],
+           diff_c = estimate[3] - estimate[4],
            lower = -1.96*(std.error/sqrt(n_obs)),
            upper = 1.96*(std.error/sqrt(n_obs)),
-           se_type = c("normal", "clustered"),
+           se_type = c("normal", "clustered","normal", "clustered"),
            i = iter,
            rho = rho)
     return(ret)
 }
 
-sim_list <- map(1:500, cluster_iter)
+sim_list <- map(1:5, cluster_iter)
 sim_df <- bind_rows(sim_list)
 
 g0 = ggplot(data = sim_df %>% filter(se_type == "normal")) +
