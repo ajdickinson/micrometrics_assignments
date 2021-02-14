d5bfa3d655322049b4db6214134a391086f14f86
Andrew Dickinson <adickin3@uoregon.edu>
2021-02-14 14:53:58 -0800

    simulation working. need to clean up visuals and code event study

--- A5/tmp02.R
+++ A5/tmp02.R
@@ -1,10 +1,13 @@
 library(pacman)
 p_load(tidyverse, ggplot2)
 
+p_num = 10
+n_obs = 100
 
 dat <- tibble(
+  time = rep(1:p_num,
+    c(rep("Before",75),rep("After",75)),2),
   control = c(rep("Control",150),rep("Treatment",150)),
-  time=rep(c(rep("Before",75),rep("After",75)),2),
   y = 2 + 2*(control == "Treatment") + 1*(time =="After") + 1.5*(control == "Treatment")*(time=="After")+rnorm(300),
   cont_time = (time == "Before") + 2*(time == "After") + (runif(300)-.5)*.95,
   dgroup = ifelse(control == "Treatment" & time == "After", "post_treat",
