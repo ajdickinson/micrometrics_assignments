b8c352607d3e49ab0c7b5498eafaa220ba68e9cd
Andrew Dickinson <adickin3@uoregon.edu>
2021-03-10 08:13:31 -0800

    sims coded (rough)

--- final_project/sims.R
+++ final_project/sims.R
@@ -1,3 +1,9 @@
+library(pacman)
+p_load(tidyverse,patchwork,kableExtra,lfe,lmtest,xtable,
+       did,magrittr,ggthemes,bacondecomp,multcomp,fastDummies,
+       ggforce)
+
+
 sim1 = function(...) {
   ## generate unit specific variation
   unit <- tibble(
@@ -82,6 +88,43 @@ sim3 = function(...) {
       group == 2000 ~ 3,
       group == 2008 ~ 3,
       group == 2016 ~ 3
+    )) %>%
+    rowwise() %>% 
+    mutate(beta = rnorm(1, beta_hat, 0.5), 0) %>% 
+    ungroup()
+  
+  ## generate year specific variation
+  year <- tibble(
+    year = 1995:2020,
+    year_fe = rnorm(26, 0, 1)
+  )
+  
+  ## Put them together
+  dat_iter = crossing(unit, year) %>% 
+    mutate(e = rnorm(nrow(.), 0, 1),
+           treat = ifelse(year >= group, 1, 0),
+           tau = ifelse(treat == 1, beta, 0),
+           y = unit_fe + year_fe + tau + e)
+  
+  return(dat_iter)
+}
+
+
+sim4 = function(...) {
+  ## generate unit specific variation
+  unit <- tibble(
+    unit = 1:1000,
+    unit_fe = rnorm(1000, 0, 1),
+    state = sample(rep(1:50, 20), 1000, replace = FALSE),
+    group = case_when(
+      state %in% 1:17 ~ 2000,
+      state %in% 18:35 ~ 2008,
+      state %in% 35:50 ~ 2016
+    ),
+    beta_hat = case_when(
+      group == 2000 ~ 5,
+      group == 2008 ~ 3,
+      group == 2016 ~ 1
     )) %>%
     rowwise() %>% 
     mutate(beta = rnorm(1, beta_hat, 0.5), 0) %>% 
