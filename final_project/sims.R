e30785287dd9b27eef1ed7d028cbf63d764964f8
Andrew Dickinson <adickin3@uoregon.edu>
2021-03-11 15:48:11 -0800

    post presentation

--- final_project/sims.R
+++ final_project/sims.R
@@ -324,3 +324,71 @@ dynamic = function(...) {
   return(dat_iter)
 }
 
+
+
+
+sim05 = function(...) {
+  ## generate unit specific variation
+  unit <- tibble(
+    unit = 1:1000,
+    unit_fe = rnorm(1000, 0, 0.5),
+    state = sample(rep(1:50, 20), 1000, replace = FALSE),
+    group = case_when(
+      state %in% 1:17 ~ 2000,
+      state %in% 18:35 ~ 2008,
+      state %in% 35:50 ~ 2016
+    ),
+    # average yearly treatment effect (by group) [\beta = 2]
+    beta_hat = case_when(
+      group == 2000 ~ .5,
+      group == 2008 ~ .3,
+      group == 2016 ~ .1
+    )) %>%
+    rowwise() %>% 
+    mutate(beta = rnorm(1, beta_hat, 0.2), 0) %>% 
+    ungroup()
+  
+  ## generate year specific variation
+  year <- tibble(
+    year = 1995:2020,
+    year_fe = rnorm(26, 0, 1)
+  )
+  
+  ## slap them together
+  dat_iter = crossing(unit, year) %>% 
+    mutate(e = rnorm(nrow(.), 0, 0.5),
+           treat = ifelse(year >= group, 1, 0),
+           tau = ifelse(treat == 1, beta, 0)) %>% 
+    ## Generate time varying dependent variable
+    group_by(unit) %>% 
+    mutate(cumtau = cumsum(tau)) %>% 
+    mutate(y = unit_fe + year_fe + cumtau + e)
+  
+  return(dat_iter)
+}
+
+data05 = sim05()
+
+
+
+# Plot 3 - GB1 ---------------------------------------------------------
+# Goodman-bacon decomp, overall information
+data <- tibble(
+  time = 0:100,
+  U = seq(5, 12, length.out = 101),
+  l = seq(10, 17, length.out = 101) + c(rep(0, 85), rep(15, 16)),
+  k = seq(18, 25, length.out = 101) + c(rep(0, 34), rep(10, 67))
+) %>% 
+  pivot_longer(-time, names_to = "series", values_to = "value")
+
+# plot
+data %>% 
+  ggplot(aes(x = time, y = value, group = series, color = series, shape = series)) + 
+  geom_line(size = 2) + geom_point(size = 2) +
+  geom_vline(xintercept = c(34, 85)) +
+  labs(x = "Time", y = "Y") +
+  scale_x_continuous(limits = c(0, 100), breaks = c(34, 85), 
+                     expand = c(0, 0)) + 
+  scale_y_continuous(limits = c(0, 40), expand = c(0, 0)) +
+  scale_color_manual(values = c("#A7473A", "#4B5F6C", "#51806a")) + 
+  theme_ipsum()
